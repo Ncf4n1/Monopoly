4e737966911b260acc83fa7a3befb1dba102ac3d
Logan Vining <llvinin@hotmail.com>
2017-11-07 11:36:40 -0700

    Updated Deeds and child property classes

--- src/esof322/a3/Railroad.java
+++ src/esof322/a3/Railroad.java
@@ -1,9 +1,20 @@
 package esof322.a3;
 
-public class Railroad extends Deed{
+public class Railroad extends Deed
+{
+	private String name;
+	private int price;
+	private Player owner;
+	private int[] rentRates;
+	private int mortgageVal;
 	
-	public Railroad(){
-		
+	public Railroad(String name)
+	{
+		this.name = name;
+		price = 200;
+		//owner = Bank
+		rentRates = new int[]{25, 50, 100, 200};
+		mortgageVal = 100;
 	}
 
 	
--- src/esof322/a3/Utility.java
+++ src/esof322/a3/Utility.java
@@ -1,9 +1,18 @@
 package esof322.a3;
 
-public class Utility extends Deed{
+public class Utility extends Deed
+{
+	private String name;
+	private int price;
+	private Player owner;
+	private int mortgageVal;
 	
-	public Utility(){
-		
+	public Utility(String name)
+	{
+		this.name = name;
+		price = 150;
+		//owner = Bank
+		mortgageVal = 75;
 	}
 	
 }
