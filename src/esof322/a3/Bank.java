7b18c928b4f03e8ce4d5163d786f488a86fb6312
Glen Johnson <glen.e.johnson@sbcglobal.net>
2017-11-07 22:01:27 -0700

    Added functionality to giveMoney

--- src/esof322/a3/Bank.java
+++ src/esof322/a3/Bank.java
@@ -1,6 +1,32 @@
 package esof322.a3;
 
 public class Bank {
+	
+	private int houses, hotels;
+	
+	public Bank () {
+		houses = 32;
+		hotels = 12;
+	}
+	
+	public void sellHouse(Property property, Player player) {
+		
+		if(player.checkForMonopoly(property) > property.getHousePrice()) {
+			
+		}
+			
+		
+	}
+	
+	public void sellHotel() {
+		
+	}
+	
+	public void giveMoney(Player player) {
+		
+		
+		
+	}
 
 	
 }
