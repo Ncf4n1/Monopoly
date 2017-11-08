0d85d55553bb1e689a961914a871d4034209b447
Glen Johnson <glen.e.johnson@sbcglobal.net>
2017-11-08 14:37:52 -0700

    Added get methods and way to add or subtract houses and hotels

--- src/esof322/a3/Bank.java
+++ src/esof322/a3/Bank.java
@@ -9,16 +9,31 @@ public class Bank {
 		hotels = 12;
 	}
 	
+	//deduct 1 from the number  of houses
 	public void sellHouse(Property property, Player player) {
 		
-		if(player.checkForMonopoly(property) > property.getHousePrice()) {
-			
-		}
-			
+		houses--;
 		
 	}
 	
+	//deduct 1 from the number of hotels
 	public void sellHotel() {
+		
+		hotels--;
+		
+	}
+	
+	//add 1 to the number  of houses
+	public void buybackHouse(Property property, Player player) {
+			
+		houses++;
+			
+	}
+		
+	//add 1 to the number of hotels
+	public void buybacHotel() {
+			
+		hotels++;
 		
 	}
 	
@@ -27,6 +42,18 @@ public class Bank {
 		
 		player.takePayment(money);
 		
+	}
+	
+	//get the number of houses left to build
+	public int getHouses()
+	{
+		return houses;
+	}
+	
+	//get the number of hotels left to build
+	public int getHotels()
+	{
+		return hotels;
 	}
 
 	
