a60cb1611fa9902b2014f489b0620fbf7b1532be
roboemily <31740786+roboemily@users.noreply.github.com>
2017-11-08 23:00:19 -0700

    start new game pane added

--- src/esof322/a3/Gui.java
+++ src/esof322/a3/Gui.java
@@ -1,5 +1,78 @@
 package esof322.a3;
 
-public class Gui {
-	// Thanks for tackling this Emily. 
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class Gui extends JFrame{
+
+  public static void main(String[] args){
+    new Gui();
+  }
+
+  public Gui() {
+    startWindow();
+  }
+
+  public void startWindow() {
+    setTitle("Monopoly");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    JPanel centerPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+    JButton newgame = new JButton("New Game");
+    newgame.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+              System.out.println("new game");
+              addOptionsPanel(centerPanel);
+            }
+        });
+    centerPanel.add(newgame);
+    JButton close = new JButton("Close Program");
+    close.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+              System.exit(0);
+            }
+        });
+    centerPanel.add(close);
+    menu();
+    add(centerPanel, BorderLayout.CENTER);
+    setSize(500, 500);
+    setVisible(true);
+  }
+
+  public void addOptionsPanel(JPanel oldPanel) {
+    JPanel optionsPanel = new JPanel();
+    JButton btn = new JButton("enter");
+    optionsPanel.add(btn);
+    oldPanel.setVisible(false);
+    add(optionsPanel, FlowLayout.CENTER);
+  }
+
+  public void menu(){
+    JMenuBar bar = new JMenuBar();
+    JMenu exitOption = new JMenu("Exit");
+    JMenuItem exit = new JMenuItem("Exit Game");
+    exit.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+              System.exit(0);
+            }
+        });
+    exitOption.add(exit);
+    bar.add(exitOption);
+    setJMenuBar(bar);
+  }
+
+  public JPanel createPanel() {
+    JPanel newpanel = new JPanel();
+    newpanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+    setSize(500,500);
+    setVisible(true);
+    return newpanel;
+  }
+
 }
