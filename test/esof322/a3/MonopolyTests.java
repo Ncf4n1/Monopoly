927cddd2d78c322948632fa3e9c980f4d3844a32
Glen Johnson <glen.e.johnson@sbcglobal.net>
2017-11-10 15:06:02 -0700

    Made tests for bank functions

--- test/esof322/a3/MonopolyTests.java
+++ test/esof322/a3/MonopolyTests.java
@@ -15,10 +15,81 @@ public class MonopolyTests {
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 	}
+	
+	@Test 
+	public void testBankSellHouse()
+	{
+		Bank bank = new Bank();
+		bank.sellHouse();
+		
+		assertEquals(31, bank.getHouses());
+	}
+	
+	@Test 
+	public void testBankBuyBackHouse()
+	{
+		Bank bank = new Bank();
+		bank.sellHouse();
+		bank.sellHouse();
+		bank.sellHouse();
+		
+		bank.buybackHouse();
+		
+		assertEquals(30, bank.getHouses());
+	}
+	
+	@Test
+	public void testBankSellHotel()
+	{
+		Bank bank = new Bank();
+		bank.sellHotel();
+		
+		assertEquals(11, bank.getHotels());
+	}
+	
+	@Test
+	public void testBankBuyBackHotel()
+	{
+		Bank bank = new Bank();
+		bank.sellHotel();
+		bank.sellHotel();
+		bank.sellHotel();
+		
+		bank.buybackHotel();
+		
+		assertEquals(10, bank.getHotels());
+	}
+	
+	@Test
+	public void testGiveMoney()
+	{
+		Player player = new Player ("Test", "Test");
+		Bank bank = new Bank();
+		
+		bank.giveMoney(player, 500);
+		
+		assertEquals(2000, player.getMoneyTotal());
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFalseCheckForMonopoly() {
+		Player player = new Player("Test", "Test");
+		Property prop = new Property("MediterraneanAvenue", 60, new int[] {2, 10, 30, 90, 160, 250}, 50, 30, 2, 1243, 1425, 0);
+		
+		player.buyProperty(prop);
+		
+		assertEquals(true, player.checkForMonopoly(prop));
+	}
+	
+	@Test
+	public void testTrueCheckForMonopoly() {
+		Player player = new Player("Test", "Test");
+		Property prop = new Property("MediterraneanAvenue", 60, new int[] {2, 10, 30, 90, 160, 250}, 50, 30, 2, 1243, 1425, 0);
+		Property prop2 = new Property("BalticAvenue", 60, new int[] {4, 20, 60, 180, 320, 450}, 50, 30, 2, 994, 1425, 0);
+		player.buyProperty(prop);
+		player.buyProperty(prop2);
+		
+		assertEquals(1380, player.getMoneyTotal());
 	}
 
 }
