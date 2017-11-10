76bd83ed85edf05300dbf23ebff7fe54aff8530e
Tonia Schlick <tonia.schlick@gmail.com>
2017-11-10 07:46:22 -0700

    commiting test file

---
+++ test/esof322/a3/MonopolyTests.java
@@ -0,0 +1,24 @@
+package esof322.a3;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MonopolyTests {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
