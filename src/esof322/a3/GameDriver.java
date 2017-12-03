d390e068d4ec04834a1e1308e334468b792ba7d5
Tonia Schlick <tonia.schlick@gmail.com>
2017-12-03 15:33:10 -0700

    impliment messages boxes

--- src/esof322/a3/GameDriver.java
+++ src/esof322/a3/GameDriver.java
@@ -20,12 +20,13 @@ public class GameDriver
 	private static int doublesInARow = 0;					// Number of times doubles has been rolled in a row
 	private static int rollTotal = 0;						// Total for the dice roll
 	private static int previousPlayerLocation = 0;			// Location of the current turn player prior to the dice roll
-	private static Property currentLandedProperty = null;	// Placeholder for a property if the player lands on it
-	private static Railroad currentLandedRailroad = null;	// Placeholder for railroad if the player lands on it
-	private static Utility currentLandedUtility = null;	// Placeholder for utility if the player lands on it
+	private static Property propLoction = null;	// Placeholder for a property if the player lands on it
+	private static Railroad rRLocation = null;	// Placeholder for railroad if the player lands on it
+	private static Utility utilLocation = null;	// Placeholder for utility if the player lands on it
     private static ArrayList<Property> propertiesAvailableToBuild = new ArrayList<>();
     private static int turns = 0;
     private static ChanceDeck chanceDeck = new ChanceDeck();
+    private static CommunityChestDeck communityDeck = new CommunityChestDeck();
 //    private static CommunityChest communityDeck = new CommunityChest();
 
 	public static void main(String[] args)
@@ -104,120 +105,172 @@ public class GameDriver
         }
     }
 
-	public static void checkSpace(int Location){
-		JOptionPane.showMessageDialog(null, rollTotal + ", " +board.getSpace(Location).getName());
-        currentLandedProperty = null;
-        currentLandedRailroad = null;
-        currentLandedUtility = null;
+	public static void checkSpace(){
+		Space location = board.getSpace(currentPlayerLocation);
+		String turnInfo = gitTurnInfo(location);
+		JOptionPane.showMessageDialog(null, die1.getDie() + " & " + die2.getDie() + ", " + location.getName());
+		propLoction = null;
+        rRLocation = null;
+        utilLocation = null;
 		// Handle which type of the square the player landed on
-		switch (currentPlayerLocation)
-		{
-			// Community Chest, Free Parking, Go, and Both Jail Squares (Do Nothing)
-			case 0: case 2:  case 10: case 17: case 20:  case 33: 
-					break;
+		switch (currentPlayerLocation){
+			 // Go Space	
+			case 0:		
+				break;
+				
+			// Visiting Jail Square
+		   case 10: 
+			   break;
+			   
+			// Free Parking
+		   case 20:  
+				break;
+				  
+		    // Chance
+		   case 7: case 22: case 36: 	 
+				chanceDeck.drawCard(players[currentPlayer], board);
+				break;
 			
-			// Cases for Chance 
-			case 7: case 22: case 36: chanceDeck.drawCard(players[currentPlayer], board);
-					break;
+			// Community Chest
+		   case 2: case 17: case 33: 	
+				communityDeck.drawCard(players[currentPlayer]);
+				break;	
 					
-			case 30: players[currentPlayer].setJailedStat(true);
-					 break;
-
-			// Cases for two Tax spaces (Make appropriate tax payment)
-			case 4: players[currentPlayer].makePayment(200);
-					break;
-			case 38: players[currentPlayer].makePayment(100);
-					break;
-
-			// Case for landing on a railroad and either paying the correct rent or buying it
-			case 5: case 15: case 25: case 35: currentLandedRailroad = (Railroad) board.getSpace(currentPlayerLocation);
-					if ( (currentLandedRailroad.getOwner() != null) && (!currentLandedRailroad.getMortgageStat()))
-					{
-                                            players[currentPlayer].makePayment(currentLandedRailroad.getRent(currentLandedRailroad.getOwner().getRailroadOwnedCount()));
-					}
-                                        else if (currentLandedRailroad.getOwner() == null)
-					{
-                                            ButtonPanel.getInstance().enablePropertyButton();
-					}
-					break;
-
-			// Case for landing on a utility and paying the correct rent based on dice roll or buying it
-			case 12: case 28: currentLandedUtility = (Utility) board.getSpace(currentPlayerLocation);
-                                        if ( (currentLandedUtility.getOwner() != null) && (!currentLandedUtility.getMortgageStat()))
-                                        {
-                                                if (currentLandedUtility.getOwner().getUtilitysOwned() == 1)
-                                                {
-                                                        players[currentPlayer].makePayment(4*rollTotal);
-                                                        currentLandedUtility.getOwner().takePayment(4*rollTotal);
-                                                }
-                                                else if (currentLandedUtility.getOwner().getUtilitysOwned() == 2)
-                                                {
-                                                        players[currentPlayer].makePayment(10*rollTotal);
-                                                        currentLandedUtility.getOwner().takePayment(10*rollTotal);
-                                                }
-                                        }
-                                        else if (currentLandedUtility.getOwner() == null)
-                                        {
-                                            ButtonPanel.getInstance().enablePropertyButton();
-                                        }
-					break;
-
+			// Got to Jail
+		   case 30: 		
+				players[currentPlayer].setJailedStat(true);
+				break;
+	
+			// Income Tax
+		   case 4: 		
+				players[currentPlayer].makePayment(((IncomeTax) location).getTaxAmount());
+				break;
+					
+			// Luxury Tax
+			case 38: 		
+				players[currentPlayer].makePayment(((LuxuryTax) location).getTaxAmount());
+				break;
+	
+			//Railroads
+			case 5: case 15: case 25: case 35:  
+				rRLocation = (Railroad) location;
+				if ( (rRLocation.getOwner() != null) && (!rRLocation.getMortgageStat())){
+					players[currentPlayer].makePayment(rRLocation.getRent(rRLocation.getOwner().getRailroadOwnedCount()));
+				}
+	            else if (rRLocation.getOwner() == null){
+	               ButtonPanel.getInstance().enablePropertyButton();
+				}
+				break;
+	
+			// Utility
+			case 12: case 28: 		
+				utilLocation = (Utility) location;
+	            if ((utilLocation.getOwner() != null) && (!utilLocation.getMortgageStat())) {                                          
+	                players[currentPlayer].makePayment(utilLocation.getOwner().getUtilitysMultiplyer()*rollTotal);
+	                utilLocation.getOwner().takePayment(utilLocation.getOwner().getUtilitysMultiplyer()*rollTotal);
+	            }
+	            else if (utilLocation.getOwner() == null){
+	                ButtonPanel.getInstance().enablePropertyButton();
+	            }
+				break;
+	
 			// Handle if the player landed on a property
-			default: currentLandedProperty = (Property) board.getSpace(currentPlayerLocation);
-
-					 // Check if the property is owned and not mortgaged and if not, buy it if the user wants
-					 if ( (currentLandedProperty.getOwner() != null) && (!currentLandedProperty.getMortgageStat()))
-					 {
-						 // Check if the property is part of a monopoly and has no houses (Must double rent)
-						 if ( (currentLandedProperty.getOwner().checkForMonopoly(currentLandedProperty.getMonoColor())) && (currentLandedProperty.getNumHouses() == 0) )
-						 {
-							 players[currentPlayer].makePayment(2*currentLandedProperty.getRent());
-							 currentLandedProperty.getOwner().takePayment(2*currentLandedProperty.getRent());
-						 }
-						 else
-						 {
-							 players[currentPlayer].makePayment(currentLandedProperty.getRent());
-							 currentLandedProperty.getOwner().takePayment(currentLandedProperty.getRent());
-						 }
-					 }
-                                         else if (currentLandedProperty.getOwner() == null)
-					 {
-                                             ButtonPanel.getInstance().enablePropertyButton();
-					 }
-				 	break;
+			default: 		
+				propLoction = (Property) location;
+				// Check if the property is owned and not mortgaged and if not, buy it if the user wants
+				if ( (propLoction.getOwner() != null) && (!propLoction.getMortgageStat())){
+					 
+					// Check if the property is part of a monopoly and has no houses (Must double rent)
+					if ( (propLoction.getOwner().checkForMonopoly(propLoction.getMonoColor())) && (propLoction.getNumHouses() == 0) ){
+						players[currentPlayer].makePayment(2*propLoction.getRent());
+						propLoction.getOwner().takePayment(2*propLoction.getRent());
+					}
+					else{
+						players[currentPlayer].makePayment(propLoction.getRent());
+						propLoction.getOwner().takePayment(propLoction.getRent());
+					}
+				}
+	            	else if (propLoction.getOwner() == null){
+	            		ButtonPanel.getInstance().enablePropertyButton();
+				}
+			 	break;
 		}
-
+		
 	}
+	
+	public static String gitTurnInfo(Space location){
+		String info = ("You have landed on: " + location.getName() + ".\n");
+		if (location instanceof Deed){	
+			Deed spaceDeed = (Deed) location;
+			Player owner = null;
+			owner = spaceDeed.getOwner();
+			if(owner != null){
+				int rent = getRentOwned(spaceDeed);	
+				String rentInfo = ("You owe " + owner + "$"+ rent + ".\n");
+				info = (info + rentInfo);
+			}
+			else{
+				int price = spaceDeed.getPrice();
+				String priceInfo = ("This is unowned. You may buy it for $"+ price + ".\n");
+				info = (info + priceInfo);
+			}
+		}
+		else{
+			int tax = 0;
+			if(location instanceof LuxuryTax){
+				LuxuryTax taxSpace = (LuxuryTax) location;
+				tax = taxSpace.getTaxAmount();
+			}
+			if(location instanceof IncomeTax){
+				IncomeTax taxSpace = (IncomeTax) location;
+				tax = taxSpace.getTaxAmount();
+			}
+			info = (info + "You are being taxed $" + tax + ".");
+		}
+		return info;
+	}
+	
+	
+	public static int getRentOwned(Deed space){
+		int rent;
+		if (space instanceof Railroad){
+			Railroad railSpace = (Railroad) space;
+			rent = railSpace.getRent(players[currentPlayer].getRailroadOwnedCount());
+		}
+		if (space instanceof Utility){
+			Utility utilSpace = (Utility) space;
+			rent = (utilSpace.getOwner().getUtilitysMultiplyer() * rollTotal);
+		}
+		else{
+			Property propSpace = (Property) space;
+			rent = propSpace.getRent();
+		}
+		return rent;
+	}
+	
 	public static void buyProperty(){
-            if (currentLandedProperty == null && currentLandedUtility == null)
-            {
-            	players[currentPlayer].buyProperty(currentLandedRailroad);
+            if (propLoction == null && utilLocation == null){
+            	players[currentPlayer].buyProperty(rRLocation);
             }
-            else if (currentLandedProperty == null && currentLandedRailroad == null)
-            {
-                players[currentPlayer].buyProperty(currentLandedUtility);
+            else if (propLoction == null && rRLocation == null){
+                players[currentPlayer].buyProperty(utilLocation);
             }
-            else if (currentLandedRailroad == null && currentLandedUtility == null)
-            {
-                players[currentPlayer].buyProperty(currentLandedProperty);
+            else if (rRLocation == null && utilLocation == null){
+                players[currentPlayer].buyProperty(propLoction);
             }
-            else
-            {
+            else{
             	System.out.println("Not bought");
             }
 	}
 
-
 	// Function used by the GUI to get the number of players
-	public static void setPlayers(Player[] players)
-	{
+	public static void setPlayers(Player[] players){
             GameDriver.players = players;
             numPlayers = players.length;
 	}
 
 	// Function used by the GUI to set the time limit of the game
-	public static void setTurnLimit(int turnLimit)
-	{
+	public static void setTurnLimit(int turnLimit){
             GameDriver.turnLimit = turnLimit;
             //endTime = System.currentTimeMillis() + (GameDriver.timeLimit*60)*1000;
 	}
@@ -226,6 +279,7 @@ public class GameDriver
 	public static Player getCurrentPlayer(){
 		return players[currentPlayer];
 	}
+	
 	public static Player[] getPlayers(){
 		return players;
 	}
@@ -278,7 +332,4 @@ public class GameDriver
         
         ImagePanel.getInstance().declareWinner(winners);
 	}
-	
-	
-	
 }
